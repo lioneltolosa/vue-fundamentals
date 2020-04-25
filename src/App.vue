89b6e64fbaba4a65fc5d4ea0ab65eb007d6713bd
Lionel Tolosa <lioneltolosaarmas@gmail.com>
2020-04-25 22:24:51 +0200

    Add SideBar and Children Paths

--- src/App.vue
+++ src/App.vue
@@ -17,9 +17,14 @@
             </ul>
         </nav>
     </header>
-    <main>
-        <router-view />
-    </main>
+    <div class="container">
+      <aside class="aside">
+        <router-view name="sidebar"/>
+      </aside>
+        <main>
+          <router-view/>
+        </main>
+    </div>
   </div>
 </template>
 
@@ -54,16 +59,15 @@ body {
 }
 
 main {
-    margin: 0 auto;
     padding: 30px;
     background-color: white;
-    width: 1024px;
+    width: 964px;
     min-height: 300px;
 }
 
 header {
   background-color: #999;
-  width: 1084px;
+  width: 1184px;
   margin: 0 auto;
 }
 ul {
@@ -79,5 +83,16 @@ ul {
 .logo {
   vertical-align: middle;
   height: 30px;
+}
+.container {
+  display: flex;
+  margin: 10px auto 0 auto;
+  justify-content: center;
+}
+.aside {
+  padding: 30px;
+  background-color: #aaa;
+  width: 100px;
+  min-height: 300px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,75 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import VueRouter from 'vue-router';
 
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
 import PartInfo from '../parts/PartsInfo.vue';
+import BrowseParts from '../parts/BrowseParts.vue';
 
-Vue.use(Router);
+import RobotHeads from '../parts/RobotHeads.vue';
+import RobotArms from '../parts/RobotArms.vue';
+import RobotTorsos from '../parts/RobotTorsos.vue';
+import RobotBases from '../parts/RobotBases.vue';
+import SideBarStandard from '../sidebar/SideBarStandard.vue';
+import SideBarBuild from '../sidebar/SideBarBuild.vue';
 
-export default new Router({
-  routes: [{
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
     path: '/',
     name: 'Home',
-    component: HomePage,
+    components: {
+      default: HomePage,
+      sidebar: SideBarStandard,
+    },
   },
   {
     path: '/build',
     name: 'Build',
-    component: RobotBuilder,
+    components: {
+      default: RobotBuilder,
+      sidebar: SideBarBuild,
+    },
+  }, {
+    path: '/parts/browse',
+    name: 'BrowseParts',
+    component: BrowseParts,
+    children: [
+      {
+        name: 'BrowseHeads',
+        path: 'heads',
+        component: RobotHeads,
+      },
+      {
+        name: 'BrowseArms',
+        path: 'arms',
+        component: RobotArms,
+      },
+      {
+        name: 'BrowseTorsos',
+        path: 'torsos',
+        component: RobotTorsos,
+      },
+      {
+        name: 'BrowseBases',
+        path: 'bases',
+        component: RobotBases,
+      },
+    ],
   },
   {
     path: '/part-info/:partType/:id',
     name: 'Parts',
     component: PartInfo,
-  }],
+    props: true,
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  routes,
 });
+
+export default router;
