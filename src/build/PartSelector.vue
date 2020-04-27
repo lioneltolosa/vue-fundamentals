9458630ced5a9198b43eca92741dfb3f85007847
Lionel Tolosa <lioneltolosaarmas@gmail.com>
2020-04-27 15:17:47 +0200

    Create Custom Directive in Vue - v-pin

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -5,14 +5,15 @@
     <!-- </router-link> -->
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span v-pin class="sale" v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
 
 <script>
 /* import availableParts from '../data/parts';
-
 const parts = availableParts.heads; */
+
+import pinDirective from '../shared/pin-directive';
 
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
@@ -25,6 +26,7 @@ function getNextValidIndex(index, length) {
 }
 
 export default {
+  directives: { pin: pinDirective },
   props: {
     parts: {
       type: Array,
@@ -91,9 +93,9 @@ export default {
   border: 3px solid #aaa;
 }
 .sale {
-  position: absolute;
+ /*  position: absolute;
   bottom: 5px;
-  right: 5px;
+  right: 5px; */
   color: white;
   background-color: red;
   padding: 3px;
